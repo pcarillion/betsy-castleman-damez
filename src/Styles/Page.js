afd210d8f3648686ab69adf211e81058c5f4aba3
Paul Carillion <pcarillion@yahoo.fr>
2022-06-15 13:43:44 +0200

    styles on biography

--- src/Styles/Page.js
+++ src/Styles/Page.js
@@ -4,6 +4,20 @@ import styled from 'styled-components'
 
 export const StyledContent = styled.div`
     p {
-        margin-bottom: 20px;
+        /* margin-bottom: 20px; */
+    }
+    table {
+        width: 100%;
+        margin: 30px 0;
+        td {
+            padding: 10px 0;
+            vertical-align: baseline;
+        }
+        td:nth-child(1) {
+            width: 30%;
+        }
+        td:nth-child(2) {
+            width: 70%;
+        }
     }
 `
