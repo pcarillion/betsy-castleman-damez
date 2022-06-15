cab260883b5d840b399157e7d9843853d251ba65
Paul Carillion <pcarillion@yahoo.fr>
2022-06-15 14:00:24 +0200

    carrousel

--- src/Components/Carrousel.js
+++ src/Components/Carrousel.js
@@ -5,17 +5,18 @@ import { StyledCarrousel } from '../Styles/Carrousel'
 import arrow from '../static/arrow.svg'
 
 const Carrousel = ({pictures, selectedPicture, callback, callbackClose}) => {
+  console.log(pictures[selectedPicture])
   return (
     <StyledCarrousel>
       <div className="corner-section">
         <img src={arrow} className="chevron close" onClick={() => callbackClose()}/>
-        <p>{pictures[selectedPicture].fields.title}</p>
-        <p>{pictures[selectedPicture].fields.description}</p>
+        <p>{pictures[selectedPicture].sys.title}</p>
+        <p>{pictures[selectedPicture].sys.description}</p>
       </div>
       <div>
         {selectedPicture !== 0 && <img src={arrow} className="chevron left" onClick={() => callback(-1)}/>}
       </div>
-      <img src={pictures[selectedPicture].fields.file.url} className="mainPicture"/>
+      <img src={pictures[selectedPicture].sys.url} className="mainPicture"/>
       <div>
         {(selectedPicture +1 !== pictures.length) && <img src={arrow} className="chevron right" onClick={() => callback(1)}/>}
       </div>
